9f8ba56e3669e7cd65f2c7571dacce14c1c3b16f
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2023-11-22 22:03:09 +0600

    Update 1929. Concatenation of Array.py

--- Array/1929. Concatenation of Array.py
+++ Array/1929. Concatenation of Array.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def buildArray(self, nums):
-        new_nums = [0]*len(nums)
+        new_nums = [0]*len(nums) #here create a new array same as given array
         for i in range (len(nums)):
-            new_nums[i]=nums[nums[i]]
+            new_nums[i]=nums[nums[i]]  #iterate the given array, find the index and put it on new array
         return new_nums
 nums = [0,2,1,5,3,4]
 s = Solution()
