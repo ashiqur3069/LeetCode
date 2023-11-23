332be3761aed68edc3bd34f4fa3df66510556397
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2023-11-23 21:38:55 +0600

    Update 1424. Diagonal Traverse II.py

--- Array/1424. Diagonal Traverse II.py
+++ Array/1424. Diagonal Traverse II.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def findDiagonalOrder(self, nums):
-        array = []
-        main_array = []
-        for i, j in enumerate(nums):
-            for k, value in enumerate(j):
-                array.append((i+k, k, value))
-        array.sort()
+        array = []  #first create a new array
+        main_array = []  # This array returns the actual answer
+        for i, j in enumerate(nums): # Iterate array by i = 0 and j = nums[index0]
+            for k, value in enumerate(j):  # now for j we do the same here k = 0 and value = 1,2,3
+                array.append((i+k, k, value)) #make a tuple and append to array
+        array.sort()  # sort the array to find the values
         for i , j , k in array:
-            main_array.append(k)
-        return main_array
+            main_array.append(k)  #iterate and add the main array list
+        return main_array 
 nums = [[1,2,3],[4,5,6],[7,8,9]]
 s = Solution()
 r = s.findDiagonalOrder(nums)
