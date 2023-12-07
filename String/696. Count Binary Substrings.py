c8d1571d3d2f9de79f936335c9357be718268fbf
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2023-12-07 19:39:54 +0600

    696. Count Binary Substrings.py

---
+++ String/696. Count Binary Substrings.py
@@ -0,0 +1,26 @@
+'''
+Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, 
+and all the 0's and all the 1's in these substrings are grouped consecutively.
+Substrings that occur multiple times are counted the number of times they occur.
+
+Example 1:
+
+Input: s = "00110011"
+Output: 6
+Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+Notice that some of these substrings repeat and are counted the number of times they occur.
+Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+'''
+
+class Solution(object):
+    def countBinarySubstrings(self, s):
+	ans, prev, cur = 0, 0, 1
+	for i in range(1, len(s)):
+		if s[i] != s[i - 1]:
+			ans += min(prev, cur)
+			prev = cur
+			cur = 1
+		else:
+			cur += 1
+	ans += min(prev, cur)
+	return ans
