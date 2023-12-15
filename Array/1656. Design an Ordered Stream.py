05f298d2c1e2236e6294aa551b79239c2b37b0c6
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2023-12-15 22:21:48 +0600

    1656. Design an Ordered Stream.py

---
+++ Array/1656. Design an Ordered Stream.py
@@ -0,0 +1,16 @@
+class OrderedStream(object):
+
+    def __init__(self, n):
+        self.stream = [None]*n
+        self.ptr = 0
+
+    def insert(self, idKey, value):
+        idKey -= 1 
+        self.stream[idKey] = value 
+        if self.ptr < idKey:
+            return []
+        else:
+            while self.ptr < len(self.stream) and self.stream[self.ptr] is not None:
+                self.ptr += 1
+            return self.stream[idKey:self.ptr]
+        
