e1166df1fef191cd57f7760ac10cdfcbf33ecab4
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2024-01-12 02:06:59 +0600

    Update 832. Flipping an Image.py

--- Array/832. Flipping an Image.py
+++ Array/832. Flipping an Image.py
@@ -9,3 +9,14 @@ class Solution(object):
                     image[i][j]=1
         return image
         
+'''for row in image:
+		l = 0 
+		r = len(row)-1
+
+		while l <= r:
+			row[l], row[r] = row[r] ^ 1, row[l] ^ 1
+			l += 1
+			r -= 1
+
+	return image
+ '''
