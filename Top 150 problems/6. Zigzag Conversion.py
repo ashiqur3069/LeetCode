db844ab1aa7a86895a52baf29afa589d9b453cff
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2024-01-23 13:45:39 +0600

    Create 6. Zigzag Conversion.py

---
+++ Top 150 problems/6. Zigzag Conversion.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1 : return s
+        res = ""
+        for r in range(numRows):
+            jump = 2 * (numRows - 1)
+            for i in range(r, len(s), jump):
+                res += s[i]
+                if (r > 0 and r < numRows - 1 and i + jump - 2*r < len(s)):
+                    res += s[i + jump - 2*r]
+        return res
+
