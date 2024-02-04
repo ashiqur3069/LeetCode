2a151b5a2dc227345f1a4a80e9783c2d35563abb
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2024-02-04 19:33:54 +0600

    Update 238. Product of Array Except Self.py

--- Top 150 problems/238. Product of Array Except Self.py
+++ Top 150 problems/238. Product of Array Except Self.py
@@ -10,3 +10,19 @@ class Solution(object):
             array[i] *= postfix
             postfix *= nums[i]
         return array
+'''
+        length = len(nums)
+        products = [1] * length
+        for i in range(1, length): # For first element we do not need to calculate
+            products[i] = products[i-1] * nums[i-1]   # We do multiplication with the i element with previous 
+                                                      # all and put it in i th position 
+
+        right = nums[-1]  # Initially took last element because no element on the right
+        for i in range(length-2, -1, -1):   # from the 2nd last element 
+                                            # we multiply i th with the right value and and increse the 
+                                            # right by multipling with the i th value
+            products[i] *= right
+            right *= nums[i]
+        
+        return products
+        '''
