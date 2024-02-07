1a59489d42fed0adb30150d3e0b5e69cffbe0434
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2024-02-07 22:53:35 +0600

    Create 242. Valid Anagram.py

---
+++ Top 150 problems/242. Valid Anagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        else:
+            s_count = {}
+            t_count = {}
+            for char in s:
+                s_count[char] = s_count.get(char, 0) + 1
+            for char in t:
+                t_count[char] = t_count.get(char, 0) + 1
+            
+            return s_count == t_count
