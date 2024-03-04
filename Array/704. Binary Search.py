0d9dd88d3181d330beb9cb7f0260cba5e677087b
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2024-03-04 16:27:05 +0600

    Create 704. Binary Search.py

---
+++ Array/704. Binary Search.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def search(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            elif target > nums[mid]:
+                l = mid + 1
+            elif target < nums[mid]:
+                r = mid -1
+        return -1
+
+        
