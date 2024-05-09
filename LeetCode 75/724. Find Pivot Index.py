996f650f818bd6d519f295044742ce753401e807
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2024-05-09 17:51:26 +0600

    Create 724. Find Pivot Index.py

---
+++ LeetCode 75/724. Find Pivot Index.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        left_sum = 0
+        right_sum = sum(nums)
+
+        for i in range(len(nums)):
+            right_sum -= nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+        return -1
+
+        
