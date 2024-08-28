399385e6805d65340175c971ddb9990df61d231f
Syed Ashiqur Rahman <112290585+ashiqur3069@users.noreply.github.com>
2024-08-29 01:09:55 +0600

    Update 2336. Smallest Number in Infinite Set.py

--- LeetCode 75/2336. Smallest Number in Infinite Set.py
+++ LeetCode 75/2336. Smallest Number in Infinite Set.py
@@ -1,3 +1,4 @@
+'''
 class SmallestInfiniteSet(object):
 
     def __init__(self):
@@ -20,3 +21,17 @@ class SmallestInfiniteSet(object):
         if num < self.next_num and num not in self.added_back_set:
             self.added_back_set.add(num)
             heappush(self.added_back_heap, num)
+'''
+#Easy approch
+class SmallestInfiniteSet(object):
+
+    def __init__(self):
+        self.present = [True for _ in range(1001)]    
+    def popSmallest(self):
+        for i in range(1,1001):
+            if self.present[i] == True:
+                self.present[i] = False
+                return i
+
+    def addBack(self, num):
+        self.present[num] = True
